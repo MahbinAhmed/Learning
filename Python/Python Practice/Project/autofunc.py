c2612d3086304bfbf8e06c72f589056949385c5d
Mahbin Ahmed <mahbinahmed10@gmail.com>
2022-10-25 18:25:22 +0600

    Fourth commit. Without C programming learning files. | Made on 25 October 2022

---
+++ Python/Python Practice/Project/autofunc.py
@@ -0,0 +1,16 @@
+import time
+import pyautogui as py
+
+# pyautogui.press(["ctrl","a"])
+# py.keyDown("win")
+# py.press("2")
+# py.keyUp("win")
+# py.write("facebook.com")
+# py.press("enter")
+# py.write("Hello")
+# py.click("enter")
+# py.press("enter")
+time.sleep(2)
+for i in range(500):
+    py.write("Assalamu Alaikum Apu")
+    py.press("enter")
